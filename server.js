8772f6c092edfee52e3cfe99593fbecfeaabd201
Javier Lopez Lopez <sjavierlopez@gmail.com>
2019-05-14 20:02:56 -0500

    Add grapql server

--- server.js
+++ server.js
@@ -1,23 +1,31 @@
-var { graphql, buildSchema } = require('graphql') ;
+let express = require('express');
+let graphqlHTTP = require('express-graphql');
+let { buildSchema } = require('graphql');
 
 // Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-    type Query {
-        hello: String
-    }
+let schema = buildSchema(`
+  type Query {
+    hello: String
+  }
 `);
 
 // The root provides a resolver function for each API endpoint
-var root = {
-    hello:()=>{
-        return 'Hello World';
-    }
-}
+let root = {
+    hello: () => {
+      return 'Hello world!';
+    },
+  };
 
 
-// Run the GraphQL query '{ hello }' and print out the response
-graphql(schema, '{ hello }', root).then((response) => {
-    console.log(response);
-})
+  const app =  express();
+
+  app.use('/graphql', graphqlHTTP({
+      schema: schema,
+      rootValue: root,
+      graphiql: true,
+  }));
+
+  app.listen(4000);
+  console.log('Running a GraphQL API server at localhost:4000/graphql');
 
 
